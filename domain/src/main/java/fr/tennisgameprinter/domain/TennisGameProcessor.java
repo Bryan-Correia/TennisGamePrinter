2bebc1f1a8818c91dc9489cdf77bfbee7e1dfcc3
Bryan Correia <wandamikan@gmail.com>
2025-03-16 21:57:24 +0100

    Make all tests in TennisGameProcessorTest pass

--- domain/src/main/java/fr/tennisgameprinter/domain/TennisGameProcessor.java
+++ domain/src/main/java/fr/tennisgameprinter/domain/TennisGameProcessor.java
@@ -2,9 +2,14 @@ package fr.tennisgameprinter.domain;
 
 import fr.tennisgameprinter.domain.game.factory.TennisGameFactory;
 import fr.tennisgameprinter.domain.game.input.TennisGameInput;
+import fr.tennisgameprinter.domain.game.player.Player;
+import fr.tennisgameprinter.domain.game.point.Point;
 import fr.tennisgameprinter.domain.ports.deserializer.InputDeserializer;
 import fr.tennisgameprinter.domain.ports.probe.GameStateProbe;
 import fr.tennisgameprinter.domain.validator.GameConsistencyValidator;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A service which processes a {@link TennisGameInput} by simulating the underlying tennis game, with the
@@ -13,6 +18,11 @@ import fr.tennisgameprinter.domain.validator.GameConsistencyValidator;
  * @param <P> the type of players' identifier.
  */
 public class TennisGameProcessor<I, P> {
+
+    private final InputDeserializer<I, P> inputDeserializer;
+    private final GameConsistencyValidator<P> gameConsistencyValidator;
+    private final GameStateProbe<P> gameStateProbe;
+    private final TennisGameFactory<P> tennisGameFactory;
 
     /**
      * Creates a new {@link TennisGameProcessor} with the provided {@link InputDeserializer}, {@link GameConsistencyValidator},
@@ -26,6 +36,10 @@ public class TennisGameProcessor<I, P> {
                                final GameConsistencyValidator<P> gameConsistencyValidator,
                                final GameStateProbe<P> gameStateProbe,
                                final TennisGameFactory<P> tennisGameFactory) {
+        this.inputDeserializer = Objects.requireNonNull(inputDeserializer, "the provided input deserializer must not be null.");
+        this.gameConsistencyValidator = Objects.requireNonNull(gameConsistencyValidator, "the provided points consistency validator must not be null.");
+        this.gameStateProbe = Objects.requireNonNull(gameStateProbe, "the provided game state probe must not be null.");
+        this.tennisGameFactory = Objects.requireNonNull(tennisGameFactory," the provided tennis game factory must not be null.");
     }
 
     /**
@@ -34,5 +48,10 @@ public class TennisGameProcessor<I, P> {
      * @param tennisGameInput the {@link TennisGameInput}.
      */
     public void process(final TennisGameInput<I, P> tennisGameInput) {
+        List<Point<P>> points = inputDeserializer.deserialize(tennisGameInput.input());
+        gameConsistencyValidator.validate(tennisGameInput.firstPlayerId(), tennisGameInput.secondPlayerId(), points);
+        Player<P> firstPlayer = new Player<>(tennisGameInput.firstPlayerId());
+        Player<P> secondPlayer = new Player<>(tennisGameInput.secondPlayerId());
+        tennisGameFactory.create(firstPlayer, secondPlayer, gameStateProbe).process(points);
     }
 }
--- domain/src/main/java/fr/tennisgameprinter/domain/game/player/Player.java
+++ domain/src/main/java/fr/tennisgameprinter/domain/game/player/Player.java
@@ -1,13 +1,21 @@
 package fr.tennisgameprinter.domain.game.player;
 
+import java.util.Objects;
+
 /**
  * A stateful representation of a tennis player. It keeps track of a player's score.
  * @param <P> the type of players' identifier.
  */
 public class Player<P> {
 
+    private final P id;
+
+    public Player(final P id) {
+        this.id = Objects.requireNonNull(id, "the provided id must not be null.");
+    }
+
     public P getId() {
-        return null;
+        return id;
     }
 
 }
