932123e5f211e964382fa0163fef6d0a7a36641f
Bryan Correia <wandamikan@gmail.com>
2025-03-16 23:48:37 +0100

    Create PlayerTest and make it compile

--- domain/src/main/java/fr/tennisgameprinter/domain/game/player/Player.java
+++ domain/src/main/java/fr/tennisgameprinter/domain/game/player/Player.java
@@ -3,23 +3,49 @@ package fr.tennisgameprinter.domain.game.player;
 import java.util.Objects;
 
 /**
- * A stateful representation of a tennis player. It keeps track of a player's score.
+ * A stateful representation of a tennis player. It keeps track of a player's score, and allows updating its state
+ * everytime a point is won against the other player.
  * @param <P> the type of players' identifier.
  */
 public class Player<P> {
 
+    /** the identifier of the player. */
     private final P id;
 
+    /**
+     * Creates a new {@link Player} with the provided identifier.
+     * @param id the identifier of the player.
+     */
     public Player(final P id) {
         this.id = Objects.requireNonNull(id, "the provided id must not be null.");
     }
 
+    /**
+     * @return the identifier of this player.
+     */
     public P getId() {
         return id;
     }
 
+    /**
+     * @return the score for this player.
+     */
     public int getScore() {
         return 0;
+    }
+
+    /**
+     * Updates the state of this player by making it win the point over the provided player.
+     * @param otherPlayer the other player against which the point was won.
+     */
+    public void winsPointOver(final Player<P> otherPlayer) {
+    }
+
+    /**
+     * @return whether this player has the advantage.
+     */
+    public boolean hasAdvantage() {
+        return false;
     }
 
 }
---
+++ domain/src/test/java/fr/tennisgameprinter/domain/game/player/PlayerTest.java
@@ -0,0 +1,66 @@
+package fr.tennisgameprinter.domain.game.player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    public void testConstructor() {
+        // WHEN we create a new player identified as 'A'
+        Player<Character> player = new Player<>('A');
+        // THEN it is identified as 'A'
+        assertEquals('A', player.getId());
+        // AND it has a score of zero
+        assertEquals(0, player.getScore());
+    }
+
+    @Test
+    public void testWinsPointOver() {
+        // GIVEN our first player identified by character 'A'
+        Player<Character> firstPlayer = new Player<>('A');
+        // AND our second player identified by character 'B'
+        Player<Character> secondPlayer = new Player<>('B');
+
+        // WHEN firstPlayer wins a point over secondPlayer
+        firstPlayer.winsPointOver(secondPlayer);
+        // THEN the score matches our expectations
+        assertEquals(15, firstPlayer.getScore());
+
+        // WHEN firstPlayer wins a second point over secondPlayer
+        firstPlayer.winsPointOver(secondPlayer);
+        // THEN the score matches our expectations
+        assertEquals(30, firstPlayer.getScore());
+
+        // WHEN firstPlayer wins a third point over secondPlayer
+        firstPlayer.winsPointOver(secondPlayer);
+        // THEN the score matches our expectations
+        assertEquals(40, firstPlayer.getScore());
+
+        // WHEN secondPlayer catches up
+        secondPlayer.winsPointOver(firstPlayer);
+        secondPlayer.winsPointOver(firstPlayer);
+        secondPlayer.winsPointOver(firstPlayer);
+        // THEN the scores match our expectations
+        assertEquals(40, firstPlayer.getScore());
+        assertEquals(40, secondPlayer.getScore());
+
+        // WHEN firstPlayer gets advantage
+        firstPlayer.winsPointOver(secondPlayer);
+        // THEN both players' state match our expectation
+        assertEquals(40, firstPlayer.getScore());
+        assertEquals(40, secondPlayer.getScore());
+        assertTrue(firstPlayer.hasAdvantage());
+        assertFalse(secondPlayer.hasAdvantage());
+
+        // WHEN firstPlayer loses advantage
+        secondPlayer.winsPointOver(firstPlayer);
+        // THEN both players' state match our expectation
+        assertEquals(40, firstPlayer.getScore());
+        assertEquals(40, secondPlayer.getScore());
+        assertFalse(firstPlayer.hasAdvantage());
+        assertFalse(secondPlayer.hasAdvantage());
+    }
+
+}
