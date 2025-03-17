a1eef71292ba6a8f44430afff5ea7f2251a46fe7
Bryan Correia <wandamikan@gmail.com>
2025-03-17 15:42:37 +0100

    Make all tests in StdoutPointPrintingListenerTest pass

--- adapters/src/main/java/fr/tennisgameprinter/adapters/listener/StdoutPointPrintingListener.java
+++ adapters/src/main/java/fr/tennisgameprinter/adapters/listener/StdoutPointPrintingListener.java
@@ -11,23 +11,27 @@ import fr.tennisgameprinter.domain.ports.listener.GameStateListener;
  */
 public class StdoutPointPrintingListener implements GameStateListener<Character> {
 
+    private static final String REGULAR_POINT_FORMAT = "Player %s : %s / Player %s : %s%n";
+    private static final String ADVANTAGE_FORMAT = "Player %s has advantage%n";
+    private static final String WIN_FORMAT = "Player %s wins the game%n";
+
     @Override
     public void onPoint(final RegularPoint<Character> regularPoint) {
-
+        System.out.printf(REGULAR_POINT_FORMAT, regularPoint.playerId(), regularPoint.playerScore(), regularPoint.otherPlayerId(), regularPoint.otherPlayerScore());
     }
 
     @Override
     public void onAdvantage(final AdvantagePoint<Character> advantagePoint) {
-
+        System.out.printf(ADVANTAGE_FORMAT, advantagePoint.advantagedPlayerId());
     }
 
     @Override
     public void onDeuce() {
-
+        System.out.println("Deuce");
     }
 
     @Override
     public void onVictory(final VictoryPoint<Character> victoryPoint) {
-
+        System.out.printf(WIN_FORMAT, victoryPoint.winner());
     }
 }
